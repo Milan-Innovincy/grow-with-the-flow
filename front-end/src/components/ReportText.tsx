0be646b4e51cb909993f9059e0347a943ad5d962
Sander Schekman <sanderschekman@gmail.com>
2021-02-12 15:52:41 +0100

    GWTF-10: minor improvement in report popup text

--- front-end/src/components/ReportText.tsx
+++ front-end/src/components/ReportText.tsx
@@ -5,7 +5,7 @@ export default class CookieStatement extends React.Component<{}, {}> {
     return (
       <div>
         <h2>Reporting an issue</h2>
-        <p>To report issues you can either call to <a href="tel:06123456789">06123456789</a> or send an email to: <a href="mailto::info@growwiththeflow.nl">info@growwiththeflow.nl</a>.</p>
+        <p>To report issues you can either call us at <a href="tel:06123456789">06123456789</a> or send an email to: <a href="mailto::info@growwiththeflow.nl">info@growwiththeflow.nl</a>.</p>
       </div>
     )
   }
