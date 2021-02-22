c75bd3a3f2c006d34b9bad7666a0928833c35d84
Sander Schekman <sanderschekman@gmail.com>
2021-02-22 15:24:44 +0100

    GWTF-6: Implements datepicker when no plot is selected

--- front-end/src/OverallSummary.tsx
+++ front-end/src/OverallSummary.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import 'date-fns'
 import { css } from '@emotion/css'
 import { DateTime } from 'luxon'
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers'
+import DateFnsUtils from '@date-io/date-fns'
+import EventEmitter from './lib/EventEmitter'
 
 import DateView from './DateView'
 import PlotListDialog from './PlotListDialog'
@@ -13,6 +17,21 @@ type Props = {
 }
 
 const OverallSummary = ({ farmerData, date, navigate, sprinklingCache }: Props) => {
+
+  const handleDateViewClick = () => {
+    document.querySelector('.MuiFormControl-root.MuiTextField-root.MuiFormControl-marginNormal button').click()
+  }
+
+  const handleDateChange = (newDate: any) => {
+    if (newDate > date) {
+      EventEmitter.emit('show-snackbar', {
+        snackbarMessage: 'Kies a.u.b. een datum in het verleden.'
+      })
+      return
+    }
+
+    window.location = `${window.location.origin}/map/${DateTime.fromJSDate(newDate).toISODate()}`
+  }
   
   return(
     <div
@@ -36,7 +55,29 @@ const OverallSummary = ({ farmerData, date, navigate, sprinklingCache }: Props) 
             margin: 0 0 6px 50px;
           `}
         >Alle Pixels</small>
-        <DateView date={date}/>
+        <div
+          onClick={handleDateViewClick}
+          className={css`
+            cursor: pointer;
+          `}>
+          <DateView date={date} />
+        </div>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              className={css`
+                display: none !important;
+              `}
+              margin="normal"
+              id="date-picker-dialog"
+              label="Date picker dialog"
+              format="yyyy-MM-dd"
+              value={date}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+            />
+          </MuiPickersUtilsProvider>
       </div>
       <div>
         <PlotListDialog
