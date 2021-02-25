03d4e09093aa01dfd187830c5e7b098a3f48ff16
Sander Schekman <sanderschekman@gmail.com>
2021-02-25 14:13:59 +0100

    fix: updates some translations

--- front-end/src/components/ReportText.tsx
+++ front-end/src/components/ReportText.tsx
@@ -4,8 +4,8 @@ export default class CookieStatement extends React.Component<{}, {}> {
   render() {
     return (
       <div>
-        <h2>Reporting an issue</h2>
-        <p>To report issues you can send an email to: <a href="mailto::growwiththeflow@deltares.nl">growwiththeflow@deltares.nl</a>.</p>
+        <h2>Contact</h2>
+        <p>U kunt contact opnemen door een email te sturen naar: <a href="mailto::growwiththeflow@deltares.nl">growwiththeflow@deltares.nl</a>.</p>
       </div>
     )
   }
